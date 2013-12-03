871c518096311f9b113fdb4e2a2e00afe18e5f02
John Marino <draco@marino.st>
2013-12-03 12:51:24 +0000

    Progress bar: Fix javascript array sort
    sort() method does not work for integers natively, it needs a defined
    function (which is sort of absurd).  This only worked by accident before.

--- src/share/poudriere/html/assets/poudriere.js
+++ src/share/poudriere/html/assets/poudriere.js
@@ -22,7 +22,7 @@ function maxcatwidth(A, B, C, D, queued) {
 	cat[1] = catwidth (B, queued);
 	cat[2] = catwidth (C, queued);
 	cat[3] = catwidth (D, queued);
-	cat.sort();
+	cat.sort(function(a,b){return a-b});
 	return (500 - cat[0] - cat[1] - cat[2]);
 }
 
