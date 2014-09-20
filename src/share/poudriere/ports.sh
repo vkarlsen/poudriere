9849faedb6d18b884450b211b7c8490401200ce5
John Marino <draco@marino.st>
2014-09-21 00:24:44 +0200

    Fix three OBE items and improve rsync porttree update option
    OBE 1: Rather than individual specify none-ports top level directories,
         just search for all directories starting with a capital letter.
         This captures the relatively new "Keywords" directory
    
    OBE 2: Add /lib to ldconfig paths.  This was added for DragonFly 3.8
    
    OBE 3: Remove NO_STAGE line on make.conf.  It caused poudriere to outright
         break this week.  I don't know why this condition existed.  It looks
         obsolete since a long time
    
    Improvement: Don't use cpdup on porttree-update.  It will sync everything.
    Even a single file takes tens of minutes to sync.  Instead, leave cpdup
    for the initial "rsync" creation, but then use true rsync for incremental
    updates.  This works much better in almost all cases.

--- src/share/poudriere/ports.sh
+++ src/share/poudriere/ports.sh
@@ -149,7 +149,7 @@ generate_makefiles() {
 	local CATEGORIES ALLPORTS CAT LVL2
 	
 	CATEGORIES=$(cd ${PTMNT} && find -s * -type d -depth 0 -maxdepth 0 \
-		-not \( -name Mk -o -name Tools -o -name Templates \))
+		-not -name \[A-Z\]\*)
 	rm -f ${PTMNT}/Makefile
 	for CAT in ${CATEGORIES}; do
 		echo "SUBDIR += ${CAT}" >> ${PTMNT}/Makefile
@@ -227,7 +227,7 @@ if [ ${UPDATE} -eq 1 ]; then
 	case ${METHOD} in
 	rsync)
 		msg "Updating the ports tree via rsync"
-		cpdup -VV -i0 ${QOP} ${DPORTS_RSYNC_LOC}/ ${PTMNT}/
+		rsync -a ${QOP} --delete ${DPORTS_RSYNC_LOC}/ ${PTMNT}/
 		bhack=$(date "+%Y-%m-%d/%H:%M")
 		pset ${PTNAME} timestamp ${bhack}
 		;;
